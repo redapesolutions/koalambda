39110437fe62f011573c4d40ed3b12c75f4cb8ec
Yu Jin <adamlyj89@gmail.com>
2018-01-30 13:27:57 +0800

    moving composer on init on function instead of during calls

--- src/kompose.ts
+++ src/kompose.ts
@@ -2,8 +2,6 @@ import {AsyncFunction, EventContext} from './interfaces'
 import koaCompose from 'koa-compose'
 
 export const kompose = (...fns: AsyncFunction[]) => {
-  fns.forEach(fn => {
-    if (typeof fn !== 'function') throw new TypeError('middleware must be a function!');
-  })
-  return (event, context, callback?) => koaCompose(fns)({event, context, callback} as EventContext)
+  const composer = koaCompose(fns)
+  return (event, context, callback?) => composer({event, context, callback} as EventContext)
 }
