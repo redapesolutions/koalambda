94f1483eb0ca60963a4023f74ed72d9bec5ab8c8
Dawid <dawidpol1@gmail.com>
2018-02-21 15:46:15 +0800

    Add some tests

--- src/http.ts
+++ src/http.ts
@@ -3,54 +3,65 @@ import isArray from 'lodash/isArray'
 import isNumber from 'lodash/isNumber'
 
 export enum HTTP_ERROR_CODES {
-  JSON_PARSING_ERROR=100,
+  JSON_PARSING_ERROR = 100,
   MISSING_BODY
 }
 
 export const jsonBody = async (ctx, next?) => {
   let body = ctx.event.body
   if (body === null || body === undefined) {
-    throw {message: `Empty body`, code: HTTP_ERROR_CODES.MISSING_BODY}
+    throw { message: `Empty body`, code: HTTP_ERROR_CODES.MISSING_BODY }
   }
   try {
-      body = JSON.parse(ctx.event.body)
+    body = JSON.parse(ctx.event.body)
   } catch (ex) {
-      throw {message: `Failed to parse body: ${ex}`, code: HTTP_ERROR_CODES.JSON_PARSING_ERROR}
+    throw { message: `Failed to parse body: ${ex}`, code: HTTP_ERROR_CODES.JSON_PARSING_ERROR }
   }
   ctx.state.body = body
   next && await next()
 }
 
-export const _makeResponse = (body: {[prop:string]: any}, statusCode = 200, cors = true, headers: {[prop:string]: any} = {}) => {
+export const _makeResponse = (body: { [prop: string]: any }, statusCode = 200, cors = true, headers: { [prop: string]: any } = {}) => {
   const resp = {
     statusCode,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      ...headers
     },
     body: JSON.stringify(body)
   }
+
   if (cors) {
     resp.headers = Object.assign(resp.headers, {
       "Access-Control-Allow-Origin": "*",
-      ...headers
     })
   }
   return resp
 }
 
-export const makeBadRequest = message => makeError(message)
-export const makeUnauthorized = message => makeError(message, 401)
-export const makeForbidden = message => makeError(message, 403)
+export const _makeBadRequest = message => _makeError(message)
+export const _makeUnauthorized = message => _makeError(message, 401)
+export const _makeForbidden = message => _makeError(message, 403)
+export const _makeRedirect = (redirectUrl: string) => {
+  return _makeResponse(
+    {},
+    303,
+    true,
+    {
+      Location: redirectUrl
+    }
+  )
+}
 
 export const _callbackBasedHttpHandleError = err => {
   console.log('Error: ', err)
-  let message: string, code: number = 400, internalCode: number = 0
+  let message = 'System error', code = 400, internalCode = 0
 
-  if(err instanceof ForbiddenError)
-    return Promise.resolve(makeForbidden(err.message))
+  if (err instanceof ForbiddenError)
+    return Promise.resolve(_makeForbidden(err.message))
 
-  if(err instanceof UnauthorizedError)
-    return Promise.resolve(makeUnauthorized(err.message))
+  if (err instanceof UnauthorizedError)
+    return Promise.resolve(_makeUnauthorized(err.message))
 
   if (isArray(err)) {
     [message, code] = err
@@ -62,22 +73,25 @@ export const _callbackBasedHttpHandleError = err => {
   }
 
   if (!code || !isNumber(code)) {
-    code = 0
-    message = 'System error'
+    code = 400
+  }
+
+  if (!internalCode || !isNumber(internalCode)) {
+    internalCode = 0
   }
 
   return Promise.resolve(
-    makeError(message, code, true, internalCode)
+    _makeError(message, code, true, internalCode)
   )
 }
 
-export const makeResponse = (statusCode = 200, cors = true, headers: {[prop:string]: any} = {}) => async (ctx, next) => {
+export const makeResponse = (statusCode = 200, cors = true, headers: { [prop: string]: any } = {}) => async (ctx, next) => {
   await next()
   ctx.state.response = _makeResponse(ctx.state.response, statusCode, cors, headers)
 }
 
 
-export const makeError = (message: string, statusCode = 400, cors = true, internalErrorCode = 0) => {
+export const _makeError = (message: string, statusCode = 400, cors = true, internalErrorCode = 0) => {
   return _makeResponse({
     message,
     code: internalErrorCode
@@ -86,26 +100,37 @@ export const makeError = (message: string, statusCode = 400, cors = true, intern
 
 export const makeSuccess = makeResponse()
 
-export const httpResponse = (successCode = 200, errorCode = 400, cors = true, headers: {[prop:string]: any} = {}) => async (ctx, next) => {
+export const httpResponse = (successCode = 200, errorCode = 400, cors = true, headers: { [prop: string]: any } = {}) => async (ctx, next) => {
   try {
     await next()
     ctx.state.response = _makeResponse(ctx.state.response, ctx.state.response.statusCode || successCode, cors, headers)
   } catch (e) {
-    ctx.state.response = _callbackBasedHttpHandleError(e)
+    ctx.state.response = await _callbackBasedHttpHandleError(e)
   }
 }
 
 export const standardHttpResponse = httpResponse()
 
+export const redirectHttpResponse = (linkLocation: string) => async (ctx, next) => {
+  try {
+    await next()
+
+    let link = get(ctx.state, linkLocation)
+    ctx.state.response = _makeResponse({}, 303, true, { Location: link })
+  } catch (e) {
+    ctx.state.response = await _callbackBasedHttpHandleError(e)
+  }
+}
+
 
 export class ForbiddenError {
   constructor(public message) {
-      Error.apply(this, arguments);
+    Error.apply(this, arguments);
   }
 }
 
 export class UnauthorizedError {
   constructor(public message) {
-      Error.apply(this, arguments);
+    Error.apply(this, arguments);
   }
 }
--- test/http.spec.js
+++ test/http.spec.js
@@ -5,13 +5,289 @@ import {
   standardHttpResponse,
   HTTP_ERROR_CODES,
   ForbiddenError,
-  UnauthorizedError
+  UnauthorizedError,
+  redirectHttpResponse,
+  _makeResponse,
+  _makeBadRequest,
+  _makeUnauthorized,
+  _makeForbidden,
+  _makeRedirect,
+  _callbackBasedHttpHandleError,
+  makeResponse,
+  _makeError,
+  makeSuccess,
+  httpResponse
 } from '../lib'
 import { expect } from 'chai'
 import noop from 'lodash/noop'
 import { spy } from 'sinon'
 
+
+
+
 describe('http', () => {
+  describe('internal functions', () => {
+    describe('_makeResponse', () => {
+      it('Should make response without cors', async () => {
+        let result = _makeResponse(
+          {
+            test: 1
+          },
+          200,
+          false,
+        )
+
+        expect(result).to.have.property('statusCode').which.equals(200)
+        expect(result).to.have.property('headers').which.has.property('Content-Type').which.equals('application/json')
+        expect(result).to.have.property('body').which.equals('{"test":1}')
+      })
+      it('Should make response with cors and should add header', async () => {
+        let result = _makeResponse(
+          {
+            test: 1
+          },
+          400,
+          true,
+          {
+            Location: 'www.wp.pl'
+          }
+        )
+
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result).to.have.property('headers')
+        expect(result.headers).to.have.property('Content-Type').which.equals('application/json')
+        expect(result.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+        expect(result.headers).to.have.property('Location').which.equals('www.wp.pl')
+        expect(result).to.have.property('body').which.equals('{"test":1}')
+      })
+    })
+    describe('_makeBadRequest', () => {
+      it('Should make BadRequest response', async () => {
+        let result = _makeBadRequest('Test')
+
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result).to.have.property('body').which.equals('{"message":"Test","code":0}')
+      })
+    })
+    describe('_makeUnauthorized', () => {
+      it('Should make Unauthorized response', async () => {
+        let result = _makeUnauthorized('Test')
+
+        expect(result).to.have.property('statusCode').which.equals(401)
+        expect(result).to.have.property('body').which.equals('{"message":"Test","code":0}')
+      })
+    })
+    describe('_makeForbidden', () => {
+      it('Should make Forbidden response', async () => {
+        let result = _makeForbidden('Test')
+
+        expect(result).to.have.property('statusCode').which.equals(403)
+        expect(result).to.have.property('body').which.equals('{"message":"Test","code":0}')
+      })
+    })
+    describe('_makeRedirect', () => {
+      it('Should make Redirect response', async () => {
+        let result = _makeRedirect('www.wp.pl')
+
+        expect(result).to.have.property('statusCode').which.equals(303)
+        expect(result).to.have.property('headers').which.has.property('Location').which.equals('www.wp.pl')
+      })
+    })
+    describe('_makeError', () => {
+      it('Should make default error', async () => {
+        let result = _makeError ('test')
+  
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result.headers).to.have.property('Content-Type').which.equals('application/json')
+        expect(result.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+        expect(result).to.have.property('body').which.equals('{"message":"test","code":0}')
+      })
+      it('Should make specified error', async () => {
+        let result = _makeError ('test', 500, true, 12)
+  
+        expect(result).to.have.property('statusCode').which.equals(500)
+        expect(result.headers).to.have.property('Content-Type').which.equals('application/json')
+        expect(result.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+        expect(result).to.have.property('body').which.equals('{"message":"test","code":12}')
+  
+      })
+    })
+    describe('_callbackBasedHttpHandleError', () => {
+      it('should return forbidden response', async () => {
+        let result = await _callbackBasedHttpHandleError(new ForbiddenError())
+
+        expect(result).to.have.property('statusCode').which.equals(403)
+      })
+      it('should return unauthorized response', async () => {
+        let result = await _callbackBasedHttpHandleError(new UnauthorizedError())
+
+        expect(result).to.have.property('statusCode').which.equals(401)
+      })
+      it('should return code 0 when is array and tha code number', async () => {
+        let result = await _callbackBasedHttpHandleError(['message', 500])
+
+        expect(result).to.have.property('statusCode').which.equals(500)
+        expect(result).to.have.property('body').which.equals('{"message":"message","code":0}')
+      })
+      it('should return code 12 when is object and 400 code number', async () => {
+        let result = await _callbackBasedHttpHandleError({message: 'ok', code: 12})
+
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result).to.have.property('body').which.equals('{"message":"ok","code":12}')
+      })
+      it('should return code 0 when is object and 400 code number if code in not a number', async () => {
+        let result = await _callbackBasedHttpHandleError({message: 'ok', code: "NotNumber"})
+
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result).to.have.property('body').which.equals('{"message":"ok","code":0}')
+      })
+
+      it('should return code 0 when is object and 400 code number if code is not defined', async () => {
+        let result = await _callbackBasedHttpHandleError(['ok', 'NotANumber'])
+
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result).to.have.property('body').which.equals('{"message":"ok","code":0}')
+      })
+
+      it('should return code 0 when is object and 400 if empty objecct is thrown', async () => {
+        let result = await _callbackBasedHttpHandleError({})
+
+        expect(result).to.have.property('statusCode').which.equals(400)
+        expect(result).to.have.property('body').which.equals('{"message":{},"code":0}')
+      })
+    })    
+  })
+  
+  describe('httpResponse middleware', () => {
+    it('Should make response in state', async () => {
+      let ctx = {
+        state: {
+        }
+      }
+
+      await httpResponse(303, 403, true, {Location: 'ok'})(ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+
+        ctx.state.response = {
+          test: 1
+        }
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(303)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+      expect(ctx.state.response).to.have.property('body').which.equals('{"test":1}')
+
+    })
+    it('Should make error response in state', async () => {
+      let ctx = {
+        state: {
+        }
+      }
+
+      await httpResponse(303, 400, true, {Location: 'ok'})(ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+
+        throw new ForbiddenError()
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(403)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+    })
+  })
+
+  describe('makeSuccess middleware', () => {
+    it('Should make response in state', async () => {
+      let ctx = {
+        state: {
+        }
+      }
+
+      await makeSuccess(ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+
+        ctx.state.response = {
+          test: 1
+        }
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(200)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+      expect(ctx.state.response).to.have.property('body').which.equals('{"test":1}')
+
+    })
+    it('Should make response in state even if body not defined', async () => {
+      let ctx = {
+        state: {
+        }
+      }
+
+      await makeSuccess(ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(200)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+    })
+  })
+
+  describe('makeResponse middleware', () => {
+    it('Should make response in state', async () => {
+      let ctx = {
+        state: {}
+      }
+
+      await makeResponse (501, true, { Location: 'www.wp.pl'}) (ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+
+        ctx.state.response = {
+          test: 1
+        }
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(501)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+      expect(ctx.state.response.headers).to.have.property('Location').which.equals('www.wp.pl')
+      expect(ctx.state.response).to.have.property('body').which.equals('{"test":1}')
+
+    })
+    it('Should make response in state even in ctx.state.response is not defined - with cors', async () => {
+      let ctx = {
+        state: {}
+      }
+
+      await makeResponse (501, true, { Location: 'www.wp.pl'}) (ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(501)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.have.property('Access-Control-Allow-Origin').which.equals('*')
+      expect(ctx.state.response.headers).to.have.property('Location').which.equals('www.wp.pl')
+
+    })
+    it('Should make response in state even in ctx.state.response is not defined - no cors', async () => {
+      let ctx = {
+        state: {}
+      }
+
+      await makeResponse (501, false, { Location: 'www.wp.pl'}) (ctx, () => {
+        expect(ctx.state).to.not.have.property('response')
+
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.equals(501)
+      expect(ctx.state.response.headers).to.have.property('Content-Type').which.equals('application/json')
+      expect(ctx.state.response.headers).to.not.have.property('Access-Control-Allow-Origin')
+      expect(ctx.state.response.headers).to.have.property('Location').which.equals('www.wp.pl')
+
+    })
+  })
 
   describe('jsonBody middleware', () => {
     it('should parse the json body and put it onto event s body', async () => {
@@ -119,7 +395,6 @@ describe('http', () => {
         })         
       });
 
-      console.log(result)
       expect(result.statusCode).to.equal(403)
     })
     it('should return unauthorized', async () => {
@@ -144,8 +419,61 @@ describe('http', () => {
         })         
       });
 
-      console.log(result)
       expect(result.statusCode).to.equal(401)
+    })
+  })
+  describe('redirectHttpResponse', () => {
+    it('Should return redirection', async() => {
+      let ctx = {
+        state: {
+          testVar: 'link'
+        }
+      }
+      await redirectHttpResponse('testVar')(ctx, () => null)
+
+      expect(ctx.state.response).to.have.property('statusCode').which.is.equal(303)
+      expect(ctx.state.response).to.have.property('headers').which.has.property('Location').which.equals('link')
+    })
+    it('Should return error', async() => {
+      let ctx = {
+        state: {
+          testVar: 'link'
+        }
+      }
+
+      await redirectHttpResponse('testVar')(ctx, () => {
+        throw new Error('Shit happend')
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.is.equal(400)
+    })
+
+    it('Should return forbiddenError', async() => {
+      let ctx = {
+        state: {
+          testVar: 'link'
+        }
+      }
+      
+      await redirectHttpResponse('testVar')(ctx, () => {
+        throw new ForbiddenError()
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.is.equal(403)
+    })
+
+    it('Should return unauthorizedError', async() => {
+      let ctx = {
+        state: {
+          testVar: 'link'
+        }
+      }
+      
+      await redirectHttpResponse('testVar')(ctx, () => {
+        throw new UnauthorizedError()
+      })
+
+      expect(ctx.state.response).to.have.property('statusCode').which.is.equal(401)
     })
   })
 })
